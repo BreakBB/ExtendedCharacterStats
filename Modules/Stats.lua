d88d9b86c834383cc3cfc1c0e0ce8c5217595d2b
Björn Böing <b.boeing@online.de>
2021-01-10 14:27:28 +0100

    [fix] Improve Outfitter support
    Fixes #89

--- Modules/Stats.lua
+++ Modules/Stats.lua
@@ -70,7 +70,11 @@ function Stats:CreateWindow()
     toggleButton:SetSize(44, 18)
     -- Move to old position if Leatrix Plus durability button is active. See #20
     if (_G.LeaPlusDB and _G.LeaPlusDB["DurabilityStatus"] == "On") or PawnInitialize then
-        toggleButton:SetPoint("TOPRIGHT", PaperDollItemsFrame, "TOPRIGHT", -38, -43)
+        if OutfitterButton then -- See #89
+            toggleButton:SetPoint("TOPRIGHT", PaperDollItemsFrame, "TOPRIGHT", -55, -16)
+        else
+            toggleButton:SetPoint("TOPRIGHT", PaperDollItemsFrame, "TOPRIGHT", -38, -4)
+        end
     else
         toggleButton:SetPoint("BOTTOMRIGHT", PaperDollItemsFrame, "BOTTOMRIGHT", -38, 87)
     end
