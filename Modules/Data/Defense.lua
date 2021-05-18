5089d3ea865d9d2aee395fefadf41aa38d5d59a2
Björn Böing <b.boeing@online.de>
2021-05-18 21:41:37 +0200

    [fix] Fix defense value calculation for Warriors
    Fixes #111

--- Modules/Data/Defense.lua
+++ Modules/Data/Defense.lua
@@ -3,7 +3,7 @@ local Data = ECSLoader:ImportModule("Data")
 ---@type DataUtils
 local DataUtils = ECSLoader:ImportModule("DataUtils")
 
-local _GetTalentModifierDefense, _GetItemModifierBlockValue
+local _Defense = {}
 
 
 ---@return number
@@ -34,23 +34,9 @@ function Data:GetDefenseValue()
         skillModifier = select(6, GetSkillLineInfo(skillIndex))
     end
 
-    skillModifier = skillModifier + _GetTalentModifierDefense()
-
     return skillRank .. " + " .. skillModifier
 end
 
----@return number
-_GetTalentModifierDefense = function()
-    local _, _, classId = UnitClass("player")
-    local mod = 0
-
-    if classId == Data.WARRIOR then
-        local _, _, _, _, points, _, _, _ = GetTalentInfo(3, 2)
-        mod = points * 2 -- 0-10 Anticipation
-    end
-
-    return mod
-end
 
 ---@return string
 function Data:GetDodgeChance()
@@ -69,13 +55,13 @@ end
 
 ---@return number
 function Data:GetBlockValue()
-    local setBonus = _GetItemModifierBlockValue()
+    local setBonus = _Defense:GetItemModifierBlockValue()
     local blockValue = GetShieldBlock() + setBonus
 
     return DataUtils:Round(blockValue, 2)
 end
 
-_GetItemModifierBlockValue = function()
+function _Defense:GetItemModifierBlockValue()
     local mod = 0
 
     if Data:HasSetBonusModifierBlockValue() then
