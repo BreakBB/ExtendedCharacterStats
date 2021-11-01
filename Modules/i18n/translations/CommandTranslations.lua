ce55c0cb177cf70402f07510c321efb83009f6e5
Iowerth <andprus@gmail.com>
2021-11-01 16:31:35 +0300

    Update CommandTranslations.lua

--- Modules/i18n/translations/CommandTranslations.lua
+++ Modules/i18n/translations/CommandTranslations.lua
@@ -7,30 +7,35 @@ local commandTranslations = {
         ["deDE"] = true,
         ["frFR"] = true,
         ["zhCN"] = true,
+        ["ruRU"] = true,
     },
     ["/ecs toggle"] = {
         ["enUS"] = true,
         ["deDE"] = true,
         ["frFR"] = true,
         ["zhCN"] = true,
+        ["ruRU"] = true,
     },
     ["Toggles the visibility of the stats window"] = {
         ["enUS"] = true,
         ["deDE"] = "Öffnet/Schließt das Attributefenster",
         ["frFR"] = "Affiche/Cache la fenêtre des statistiques ECS",
         ["zhCN"] = "切换统计信息窗口",
+        ["ruRU"] = "Открыть окно характеристик",
     },
     ["/ecs config"] = {
         ["enUS"] = true,
         ["deDE"] = true,
         ["frFR"] = true,
         ["zhCN"] = true,
+        ["ruRU"] = true,
     },
     ["Shows the configuration window"] = {
         ["enUS"] = true,
         ["deDE"] = "Öffnet/Schließt das Einstellungsfenster",
         ["frFR"] = "Affiche la fenêtre de configuration ECS",
         ["zhCN"] = "显示配置窗口",
+        ["ruRU"] = "Открыть окно настроек",
     },
 }
 
