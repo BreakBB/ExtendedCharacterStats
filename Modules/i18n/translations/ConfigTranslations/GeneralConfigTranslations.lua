508a0bd2c1dad2a5137bd84f269c2189bcaa7499
Iowerth <andprus@gmail.com>
2021-11-07 11:14:51 +0300

    [feature] Add Russian translation (Big thanks to @Iowerth)

--- Modules/i18n/translations/ConfigTranslations/GeneralConfigTranslations.lua
+++ Modules/i18n/translations/ConfigTranslations/GeneralConfigTranslations.lua
@@ -7,24 +7,28 @@ local generalConfigTranslations = {
         ["deDE"] = "Allgemeine Werte anzeigen",
         ["frFR"] = "Général",
         ["zhCN"] = "显示常规属性信息",
+        ["ruRU"] = "Основные характеристики",
     },
     ["Shows/Hides all general stats."] = {
         ["enUS"] = true,
         ["deDE"] = "Zeigt/Versteckt die allgemeinen Werte.",
         ["frFR"] = "Affiche/cache toutes les statistiques générales.",
         ["zhCN"] = "显示/隐藏 所有常规属性信息。",
+        ["ruRU"] = "Показать/скрыть все основные характеристики",
     },
     ["Show Movement Speed"] = {
         ["enUS"] = true,
         ["deDE"] = "Bewegungsgeschwindigkeit anzeigen",
         ["frFR"] = "Affiche la vitesse de déplacement",
         ["zhCN"] = "显示移动速度",
+        ["ruRU"] = "Скорость движения",
     },
     ["Shows/Hides the movement speed value."] = {
         ["enUS"] = true,
         ["deDE"] = "Zeigt/Versteckt die Bewegungsgeschwindigkeit.",
         ["frFR"] = "Affiche/Cache la vitesse de déplacement.",
         ["zhCN"] = "显示/隐藏 移动速度",
+        ["ruRU"] = "Показать/скрыть значение скорости движения",
     },
 }
 
