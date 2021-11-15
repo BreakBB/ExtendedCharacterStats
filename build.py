7c6264cdbede6cc49898c7239afe78d0f4c14518
Björn Böing <b.boeing@online.de>
2021-11-15 21:22:51 +0100

    Update build script

--- build.py
+++ build.py
@@ -1,65 +1,77 @@
 #!/usr/bin/env python3
 
-import sys
+import fileinput
 import os
 import shutil
 import subprocess
+import sys
 
 '''
 This program accepts one optional command line option:
 
-interfaceVersion    If provided sets the interface version to the Era or BCC version
+release If provided this will create release ready ZIP files
 
-                    Default: 'bcc'
-
-Example usage:
-
-'python build.py era'
-
-This will create a new Classic release in 'releases/<version>-classic-<latestCommit>'
+        Default: 'False'
 
 '''
-interfaceVersion = 'bcc'
 addonDir = 'ExtendedCharacterStats'
-versionDir = None
+isReleaseBuild = False
+classicInterfaceVersion = "11401"
 
 
 def main():
-    version_dir, addon_dir, zip_name = get_args()
-
-    if os.path.isdir('releases/%s' % version_dir):
-        print("Warning: Folder already exists, removing!")
-        shutil.rmtree('releases/%s' % version_dir)
-
-    release_folder_path = 'releases/%s/%s' % (version_dir, addon_dir)
-
-    copy_content_to(release_folder_path)
-    zip_release_folder(zip_name, version_dir, addon_dir)
-
-    print('New release "%s" created successfully' % version_dir)
-
-
-def get_args():
-    global interfaceVersion
-    global addonDir
-    global versionDir
-
+    global isReleaseBuild
     if len(sys.argv) > 1:
-        interfaceVersion = sys.argv[1]
+        isReleaseBuild = sys.argv[1]
+        print("Creating a release build")
 
+    universal_version_dir, tbc_version_dir, era_version_dir = get_version_dirs(isReleaseBuild)
+
+    if os.path.isdir('releases/%s' % universal_version_dir):
+        print("Warning: Folder already exists, removing!")
+        shutil.rmtree('releases/%s' % universal_version_dir)
+
+    release_folder_path = 'releases/%s' % universal_version_dir
+    release_addon_folder_path = release_folder_path + ('/%s' % addonDir)
+
+    copy_content_to(release_addon_folder_path)
+
+    zip_name = '%s-%s' % (addonDir, tbc_version_dir)
+    zip_release_folder(zip_name, universal_version_dir, addonDir)
+
+    zip_name = '%s-%s' % (addonDir, era_version_dir)
+
+    for line in fileinput.input(release_addon_folder_path + "/ExtendedCharacterStats.toc", inplace=True):
+        if line.startswith("## Interface"):
+            print("## Interface: {}\n".format(classicInterfaceVersion), end="")
+        else:
+            print(line, end="")
+
+    zip_release_folder(zip_name, universal_version_dir, addonDir)
+
+    print('New release "%s" created successfully' % universal_version_dir)
+
+
+def get_version_dirs(is_release_build):
     version, nr_of_commits, recent_commit = get_git_information()
     print("Tag: " + version)
-    versionDir = "%s-%s-%s" % (version, interfaceVersion, recent_commit)
+    if is_release_build:
+        tbc_version_dir = "%s-tbc" % version
+        era_version_dir = "%s-era" % version
+        universal_version_dir = "%s" % version
+    else:
+        tbc_version_dir = "%s-tbc-%s" % (version, recent_commit)
+        era_version_dir = "%s-era-%s" % (version, recent_commit)
+        universal_version_dir = "%s-%s" % (version, recent_commit)
 
     print("Number of commits since tag: " + nr_of_commits)
     print("Most Recent commit: " + recent_commit)
     branch = get_branch()
     if branch != "master":
-        versionDir += "-%s" % branch
+        tbc_version_dir += "-%s" % branch
     print("Current branch: " + branch)
-    zip_name = '%s-%s' % (addonDir, versionDir)
 
-    return versionDir, addonDir, zip_name
+    return universal_version_dir, tbc_version_dir, era_version_dir
 
 
 directoriesToSkip = ['.git', '.github', '.history', '.idea', 'releases']
@@ -87,7 +99,6 @@ def zip_release_folder(zip_name, version_dir, addon_dir):
 def get_git_information():
     if is_tool("git"):
         script_dir = os.path.dirname(os.path.realpath(__file__))
-        tag_string = ''
         p = subprocess.check_output(["git", "describe", "--tags", "--long"], cwd=script_dir, stderr=subprocess.STDOUT)
         tag_string = str(p).rstrip("\\n'").lstrip("b'")
 
