85eed6dacfba8a5e6df89c8fcc65c9d74c82d343
Björn Böing <b.boeing@online.de>
2022-09-12 20:39:49 +0200

    [fix] Fix hit bonus of "Dual Wielding Specialization" Shaman talent
    Fixes #196

--- Modules/Data/Melee.lua
+++ Modules/Data/Melee.lua
@@ -60,14 +60,14 @@ function _Melee:GetHitTalentBonus()
         mod = points * 1 -- 0-3% Focused Aim
     end
 
-    if classId == Data.SHAMAN then
-        local _, _, _, _, naturesGuidance, _, _, _ = GetTalentInfo(3, 6)
-        mod = naturesGuidance * 1 -- 0-3% Nature's Guidance
-
+    if ECS.IsWotlk and classId == Data.SHAMAN then
         if ECS.IsWotlk and Data:GetMeleeAttackSpeedOffHand() > 0 then
-            local _, _, _, _, dualWielding, _, _, _ = GetTalentInfo(2, 17)
+            local _, _, _, _, dualWielding, _, _, _ = GetTalentInfo(2, 19)
             mod = mod + dualWielding * 2 -- 0-6% Dual Wielding Specialization
         end
+    else
+        local _, _, _, _, naturesGuidance, _, _, _ = GetTalentInfo(3, 6)
+        mod = naturesGuidance * 1 -- 0-3% Nature's Guidance
     end
 
     if classId == Data.PALADIN then
