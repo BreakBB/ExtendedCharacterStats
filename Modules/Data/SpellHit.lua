8c0081ec1dfa6bbbe07412e416abbe5c47349420
Björn Böing <b.boeing@online.de>
2022-11-26 09:56:39 +0100

    [fix] Fix Restro Shamans Spell Hit
    Fixes #232

--- Modules/Data/SpellHit.lua
+++ Modules/Data/SpellHit.lua
@@ -76,11 +76,13 @@ function _SpellHit:GetTalentSpellHitBonus()
     if classId == Data.SHAMAN then
         if ECS.IsWotlk then
             local _, _, _, _, points, _, _, _ = GetTalentInfo(1, 16)
-            bonus = points -- 0-3% from Elemental Precision
-        end
+                bonus = points -- 0-3% from Elemental Precision
+            end
 
-        local _, _, _, _, points, _, _, _ = GetTalentInfo(3, 6)
-        bonus = bonus + points * 1 -- 0-3% Nature's Guidance
+        if (not ECS.IsWotlk) then
+            local _, _, _, _, points, _, _, _ = GetTalentInfo(3, 6)
+            bonus = bonus + points * 1 -- 0-3% Nature's Guidance
+        end
     end
 
     if classId == Data.DRUID then
