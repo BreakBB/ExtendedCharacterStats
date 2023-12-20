f9c624421d1510fcd01769fc6bc440da4fc779ad
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2023-12-20 18:08:01 -0500

    Update DefenseConfigTranslations.lua

--- Modules/i18n/translations/ConfigTranslations/DefenseConfigTranslations.lua
+++ Modules/i18n/translations/ConfigTranslations/DefenseConfigTranslations.lua
@@ -8,6 +8,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Défense Général",
         ["zhCN"] = "显示防御属性",
         ["ruRU"] = "Защита",
+        ["esES"] = "Mostrar estadísticas de defensa",
+        ["esMX"] = "Mostrar estadísticas de defensa",
     },
     ["Shows/Hides all defense stats."] = {
         ["enUS"] = true,
@@ -15,6 +17,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Affiche/cache toutes les statistiques de défense.",
         ["zhCN"] = "显示/隐藏 所有防御属性",
         ["ruRU"] = "Показать/скрыть все характеристики защиты",
+        ["esES"] = "Muestra/oculta todas estadísticas de defensa",
+        ["esMX"] = "Muestra/oculta todas estadísticas de defensa",
     },
     ["Shows/Hides the armor value."] = {
         ["enUS"] = true,
@@ -22,6 +26,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Affiche/cache la valeur de l'armure.",
         ["zhCN"] = "显示/隐藏 护甲值",
         ["ruRU"] = "Показать/скрыть значение брони",
+        ["esES"] = "Muestra/oculta el valor de armadura",
+        ["esMX"] = "Muestra/oculta el valor de armadura",
     },
     ["Shows/Hides the percentage of being crit immune."] = {
         ["enUS"] = true,
@@ -29,6 +35,8 @@ local defenseConfigTranslations = {
         ["frFR"] = false,
         ["zhCN"] = false,
         ["ruRU"] = "Показать/скрыть шанс невосприимчивости к критическому урону",
+        ["esES"] = "Muestra/oculta el porcentaje de inmunidad a los críticos.",
+        ["esMX"] = "Muestra/oculta el porcentaje de inmunidad a los críticos.",
     },
     ["Shows/Hides the reduction percentage of being critically hit."] = {
         ["enUS"] = true,
@@ -36,6 +44,8 @@ local defenseConfigTranslations = {
         ["frFR"] = false,
         ["zhCN"] = false,
         ["ruRU"] = "Показать/скрыть шанс снижения вероятности получить критический урон",
+        ["esES"] = "Muestra/oculta el porcentaje de reducción de ser golpeado críticamente.",
+        ["esMX"] = "Muestra/oculta el porcentaje de reducción de ser golpeado críticamente.",
     },
     ["Shows/Hides the defense value."] = {
         ["enUS"] = true,
@@ -43,6 +53,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Affiche/cache la valeur de défense.",
         ["zhCN"] = "显示/隐藏 防御值",
         ["ruRU"] = "Показать/скрыть значение защиты",
+        ["esES"] = "Muestra/oculta el valor de defensa.",
+        ["esMX"] = "Muestra/oculta el valor de defensa.",
     },
     ["Shows/Hides the defense rating."] = {
         ["enUS"] = true,
@@ -50,6 +62,8 @@ local defenseConfigTranslations = {
         ["frFR"] = false,
         ["zhCN"] = false,
         ["ruRU"] = "Показать/скрыть значение дополнительной защиты",
+        ["esES"] = "Muestra/oculta el la calificación de defensa.",
+        ["esMX"] = "Muestra/oculta el la calificación de defensa.",
     },
     ["Shows/Hides the block chance."] = {
         ["enUS"] = true,
@@ -57,6 +71,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Affiche/cache les chances de bloquer.",
         ["zhCN"] = "显示/隐藏 格挡几率",
         ["ruRU"] = "Показать/скрыть значение шанса блокирования",
+        ["esES"] = "Muestra/oculta la probababilidad de bloquear.",
+        ["esMX"] = "Muestra/oculta la probababilidad de bloquear.",
     },
     ["Shows/Hides the block value."] = {
         ["enUS"] = true,
@@ -64,6 +80,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Affiche/cache la valeur de bloquage.",
         ["zhCN"] = "显示/隐藏 格挡值",
         ["ruRU"] = "Показать/скрыть значение рейтинга блокирования",
+        ["esES"] = "Muestra/oculta el valor de bloqueo.",
+        ["esMX"] = "Muestra/oculta el valor de bloqueo.",
     },
     ["Shows/Hides the parry chance."] = {
         ["enUS"] = true,
@@ -71,6 +89,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Affiche/cache les chances de parer.",
         ["zhCN"] = "显示/隐藏 招架几率",
         ["ruRU"] = "Показать/скрыть шанс парирования атак",
+        ["esES"] = "Muestra/oculta la probabilidad de parar.",
+        ["esMX"] = "Muestra/oculta la probabilidad de parar.",
     },
     ["Shows/Hides the dodge chance."] = {
         ["enUS"] = true,
@@ -78,6 +98,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Affiche/cache les chances d'esquiver.",
         ["zhCN"] = "显示/隐藏 躲闪几率",
         ["ruRU"] = "Показать/скрыть шанс уклонения от атак",
+        ["esES"] = "Muestra/oculta la probabilidad de esquivar.",
+        ["esMX"] = "Muestra/oculta la probabilidad de esquivar.",
     },
     ["Shows/Hides the resilience value."] = {
         ["enUS"] = true,
@@ -85,6 +107,8 @@ local defenseConfigTranslations = {
         ["frFR"] = "Affiche/cache la valeur de résilience.",
         ["zhCN"] = false,
         ["ruRU"] = "Показать/скрыть значение устойчивости",
+        ["esES"] = "Muestra/oculta el valor de temple.",
+        ["esMX"] = "Muestra/oculta el valor de temple.",
     },
     ["Shows/Hides the total avoidance."] = {
         ["enUS"] = true,
@@ -92,6 +116,8 @@ local defenseConfigTranslations = {
         ["frFR"] = false,
         ["zhCN"] = false,
         ["ruRU"] = false,
+        ["esES"] = "Muestra/oculta eludir.",
+        ["esMX"] = "Muestra/oculta eludir.",
     },
 }
 
