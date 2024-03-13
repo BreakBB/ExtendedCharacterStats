10da1f5aa214e00f86d0a3b7a7ebdff826b8ce99
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-03-13 03:20:01 -0400

    Update ManaConfigTranslations.lua (#296)

--- Modules/i18n/translations/ConfigTranslations/ManaConfigTranslations.lua
+++ Modules/i18n/translations/ConfigTranslations/ManaConfigTranslations.lua
@@ -5,11 +5,12 @@ local manaConfigTranslations = {
     ["Show Mana Stats"] = {
         ["enUS"] = true,
         ["deDE"] = "Manawerte anzeigen",
-        ["frFR"] = "Mana Général",
+        ["frFR"] = "Afficher mana",
         ["zhCN"] = "显示法力回复",
         ["ruRU"] = "Мана",
         ["esES"] = "Mostrar maná",
         ["esMX"] = "Mostrar maná",
+        ["ptBR"] = "Mostrar mana"
     },
     ["Shows/Hides all mana stats."] = {
         ["enUS"] = true,
@@ -19,15 +20,17 @@ local manaConfigTranslations = {
         ["ruRU"] = "Показать/скрыть все характеристики маны",
         ["esES"] = "Muestra/oculta todas estadísticas de maná",
         ["esMX"] = "Muestra/oculta todas estadísticas de maná",
+        ["ptBR"] = "Mostra/oculta todas as estatísticas de mana"
     },
     ["MP5 Items"] = {
         ["enUS"] = true,
         ["deDE"] = "MP5 Gegenstände",
-        ["frFR"] = "MP5 Objets",
+        ["frFR"] = "MP5 (Objets)",
         ["zhCN"] = "装备回蓝",
         ["ruRU"] = "MP5 (экипировка)",
         ["esES"] = "MP5 (Objetos)",
         ["esMX"] = "MP5 (Objetos)",
+        ["ptBR"] = "MP5 (Itens)"
     },
     ["Shows/Hides the MP5 value from items."] = {
         ["enUS"] = true,
@@ -37,15 +40,17 @@ local manaConfigTranslations = {
         ["ruRU"] = "Показать/скрыть значение MP5 (восполнение маны каждые 5 сек вне боя) от экипировки",
         ["esES"] = "Muestra/oculta el valor de MP5 de objetos",
         ["esMX"] = "Muestra/oculta el valor de MP5 de objetos",
+        ["ptBR"] = "Mostra/oculta o valor de MP5 de itens"
     },
     ["MP5 Buffs"] = {
         ["enUS"] = true,
         ["deDE"] = "MP5 Stärkungszauber",
-        ["frFR"] = "MP5 Amélioration",
+        ["frFR"] = "MP5 (Amélioration)",
         ["zhCN"] = "增益回蓝",
         ["ruRU"] = "MP5 (баффы)",
         ["esES"] = "MP5 (Beneficios)",
         ["esMX"] = "MP5 (Beneficios)",
+        ["ptBR"] = "MP5 (Bônus)"
     },
     ["Shows/Hides the MP5 value from buffs."] = {
         ["enUS"] = true,
@@ -55,6 +60,7 @@ local manaConfigTranslations = {
         ["ruRU"] = "Показать/скрыть значение MP5 (восполнение маны каждые 5 сек вне боя) от баффов",
         ["esES"] = "Muestra/oculta el valor de MP5 de beneficios",
         ["esMX"] = "Muestra/oculta el valor de MP5 de beneficios",
+        ["ptBR"] = "Mostra/oculta o valor de MP5 de bônus"
     },
     ["MP5 Spirit"] = {
         ["enUS"] = true,
@@ -64,6 +70,7 @@ local manaConfigTranslations = {
         ["ruRU"] = "MP5 (дух)",
         ["esES"] = "MP5 (Espíritu)",
         ["esMX"] = "MP5 (Espíritu)",
+        ["ptBR"] = "MP5 (Espírito)",
     },
     ["Shows/Hides the MP5 value from spirit."] = {
         ["enUS"] = true,
@@ -73,33 +80,37 @@ local manaConfigTranslations = {
         ["ruRU"] = "Показать/скрыть значение MP5 (восполнение маны каждые 5 сек вне боя) от духа",
         ["esES"] = "Muestra/oculta el valor de MP5 de espíritu",
         ["esMX"] = "Muestra/oculta el valor de MP5 de espíritu",
+        ["ptBR"] = "Mostra/oculta o valor de MP5 de espírito",
     },
     ["MP5 Casting"] = {
         ["enUS"] = true,
         ["deDE"] = "MP5 Zaubernd",
-        ["frFR"] = "MP5 Lancer",
+        ["frFR"] = "MP5 (Incantation)",
         ["zhCN"] = "附魔回蓝",
         ["ruRU"] = "MP5 (каст)",
         ["esES"] = "MP5 (Lanzar)",
         ["esMX"] = "MP5 (Lanzar)",
+        ["ptBR"] = "MP5 (Lançar)"
     },
     ["Shows/Hides the MP5 while casting value."] = {
         ["enUS"] = true,
         ["deDE"] = "Zeigt/Versteckt den MP5-Wert während des Zauberwirkens.",
-        ["frFR"] = "Affiche/cache la valeur MP5 pendant le lancer.",
+        ["frFR"] = "Affiche/cache la valeur MP5 pendant l'incantation.",
         ["zhCN"] = "显示/隐藏 附魔回蓝",
         ["ruRU"] = "Показать/скрыть значение MP5 (восполнение маны каждые 5 сек вне боя) во время произношения заклинаний",
         ["esES"] = "Muestra/oculta el valor de MP5 mientras lanzas",
         ["esMX"] = "Muestra/oculta el valor de MP5 mientras lanzas",
+        ["ptBR"] = "Mostra/oculta o valor de MP5 durante o lançamento"
     },
     ["Shows/Hides the MP5 value when outside of the 5 second rule."] = {
         ["enUS"] = true,
         ["deDE"] = "Zeigt/Versteckt den MP5-Wert der die Mana-Regeneration außerhalb der 5-Sekunden-Regel angibt.",
-        ["frFR"] = false,
+        ["frFR"] = "Affiche/cache la valeur MP5 lorsque hors de la règle des 5 secondes.",
         ["zhCN"] = false,
         ["ruRU"] = "Показать/скрыть значение MP5 (восполнение маны каждые 5 сек вне боя) не во время произношения заклинаний",
         ["esES"] = "Muestra/oculta el valor de MP5 cuando estás fuera de la regla de los 5 segundos",
         ["esMX"] = "Muestra/oculta el valor de MP5 cuando estás fuera de la regla de los 5 segundos",
+        ["ptBR"] = "Mostra/oculta o valor de MP5 quando fora da regra dos 5 segundos"
     },
 }
 
