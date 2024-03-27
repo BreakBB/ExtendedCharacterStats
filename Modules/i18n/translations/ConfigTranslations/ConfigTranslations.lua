44ce9fbb156acd2fa3682cf1e295c241296e6802
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-03-27 07:37:22 -0400

    Update ConfigTranslations.lua

--- Modules/i18n/translations/ConfigTranslations/ConfigTranslations.lua
+++ Modules/i18n/translations/ConfigTranslations/ConfigTranslations.lua
@@ -48,9 +48,9 @@ local configTranslations = {
         ["frFR"] = "Cache ECS quand la fenêtre du personnage est ouverte",
         ["zhCN"] = "显示ECS切换按键",
         ["ruRU"] = "Скрывать ECS",
-        ["esES"] = "Oculta ECS cuando abriendo la información del personaje",
-        ["esMX"] = "Oculta ECS cuando abriendo la información del personaje",
-        ["ptBR"] = "Oculta ECS ao abrir as informações do personagem"
+        ["esES"] = "Ocultar ECS cuando abriendo la información del personaje",
+        ["esMX"] = "Ocultar ECS cuando abriendo la información del personaje",
+        ["ptBR"] = "Ocultar ECS ao abrir as informações do personagem"
     },
     ["Hides the stats windows when opening the character tab."] = {
         ["enUS"] = true,
