8369f86b8cf68132cb333805622c5633f1bfb684
BreakBB <b.boeing@online.de>
2024-11-12 09:08:47 +0100

    [fix] Correctly process PLAYER_MOUNT_DISPLAY_CHANGED and UPDATE_SHAPESHIFT_FORM events
    Fixes #193
    Fixes #249

--- Modules/EventHandler.lua
+++ Modules/EventHandler.lua
@@ -48,6 +48,10 @@ function EventHandler.HandleOnEvent(_, event, ...)
         C_Timer.After(0.5, function ()
             Stats:UpdateInformation()
         end)
+    elseif event == "PLAYER_MOUNT_DISPLAY_CHANGED" or event == "UPDATE_SHAPESHIFT_FORM" then
+        C_Timer.After(0.5, function ()
+            Stats:UpdateInformation()
+        end)
     elseif event == "INSPECT_READY" then
         GearInfos:UpdateInspectGearColorFrames()
     end
