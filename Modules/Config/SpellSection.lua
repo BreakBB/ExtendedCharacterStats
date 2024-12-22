1ec386fadb7d3da907841182c101de8cc9e5f80d
BreakBB <b.boeing@online.de>
2024-12-22 19:16:33 +0100

    Correctly disable sub functions of spell

--- Modules/Config/SpellSection.lua
+++ Modules/Config/SpellSection.lua
@@ -200,7 +200,7 @@ function _Config:LoadSpellSection()
                         name = function() return i18n("Healing Power") end,
                         desc = function() return i18n("Shows/Hides the healing power value.") end,
                         width = 1.5,
-                        disabled = function() return (not ExtendedCharacterStats.profile.spellBonus.display); end,
+                        disabled = function() return (not ExtendedCharacterStats.profile.spell.display); end,
                         get = function () return ExtendedCharacterStats.profile.spellBonus.bonusHealing.display; end,
                         set = function (_, value)
                             ExtendedCharacterStats.profile.spellBonus.bonusHealing.display = value
@@ -213,7 +213,7 @@ function _Config:LoadSpellSection()
                         name = function() return i18n("Arcane Damage") end,
                         desc = function() return i18n("Shows/Hides the arcane damage value.") end,
                         width = 1.5,
-                        disabled = function() return (not ExtendedCharacterStats.profile.spellBonus.display); end,
+                        disabled = function() return (not ExtendedCharacterStats.profile.spell.display); end,
                         get = function () return ExtendedCharacterStats.profile.spellBonus.arcaneDmg.display; end,
                         set = function (_, value)
                             ExtendedCharacterStats.profile.spellBonus.arcaneDmg.display = value
@@ -226,7 +226,7 @@ function _Config:LoadSpellSection()
                         name = function() return i18n("Fire Damage") end,
                         desc = function() return i18n("Shows/Hides the fire damage value.") end,
                         width = 1.5,
-                        disabled = function() return (not ExtendedCharacterStats.profile.spellBonus.display); end,
+                        disabled = function() return (not ExtendedCharacterStats.profile.spell.display); end,
                         get = function () return ExtendedCharacterStats.profile.spellBonus.fireDmg.display; end,
                         set = function (_, value)
                             ExtendedCharacterStats.profile.spellBonus.fireDmg.display = value
@@ -239,7 +239,7 @@ function _Config:LoadSpellSection()
                         name = function() return i18n("Frost Damage") end,
                         desc = function() return i18n("Shows/Hides the frost damage value.") end,
                         width = 1.5,
-                        disabled = function() return (not ExtendedCharacterStats.profile.spellBonus.display); end,
+                        disabled = function() return (not ExtendedCharacterStats.profile.spell.display); end,
                         get = function () return ExtendedCharacterStats.profile.spellBonus.frostDmg.display; end,
                         set = function (_, value)
                             ExtendedCharacterStats.profile.spellBonus.frostDmg.display = value
@@ -252,7 +252,7 @@ function _Config:LoadSpellSection()
                         name = function() return i18n("Holy Damage") end,
                         desc = function() return i18n("Shows/Hides the holy damage value.") end,
                         width = 1.5,
-                        disabled = function() return (not ExtendedCharacterStats.profile.spellBonus.display); end,
+                        disabled = function() return (not ExtendedCharacterStats.profile.spell.display); end,
                         get = function () return ExtendedCharacterStats.profile.spellBonus.holyDmg.display; end,
                         set = function (_, value)
                             ExtendedCharacterStats.profile.spellBonus.holyDmg.display = value
@@ -265,7 +265,7 @@ function _Config:LoadSpellSection()
                         name = function() return i18n("Nature Damage") end,
                         desc = function() return i18n("Shows/Hides the nature damage value.") end,
                         width = 1.5,
-                        disabled = function() return (not ExtendedCharacterStats.profile.spellBonus.display); end,
+                        disabled = function() return (not ExtendedCharacterStats.profile.spell.display); end,
                         get = function () return ExtendedCharacterStats.profile.spellBonus.natureDmg.display; end,
                         set = function (_, value)
                             ExtendedCharacterStats.profile.spellBonus.natureDmg.display = value
@@ -278,7 +278,7 @@ function _Config:LoadSpellSection()
                         name = function() return i18n("Physical Damage") end,
                         desc = function() return i18n("Shows/Hides the physical damage value.") end,
                         width = 1.5,
-                        disabled = function() return (not ExtendedCharacterStats.profile.spellBonus.display); end,
+                        disabled = function() return (not ExtendedCharacterStats.profile.spell.display); end,
                         get = function () return ExtendedCharacterStats.profile.spellBonus.physicalDmg.display; end,
                         set = function (_, value)
                             ExtendedCharacterStats.profile.spellBonus.physicalDmg.display = value
@@ -291,7 +291,7 @@ function _Config:LoadSpellSection()
                         name = function() return i18n("Shadow Damage") end,
                         desc = function() return i18n("Shows/Hides the shadow damage value.") end,
                         width = 1.5,
-                        disabled = function() return (not ExtendedCharacterStats.profile.spellBonus.display); end,
+                        disabled = function() return (not ExtendedCharacterStats.profile.spell.display); end,
                         get = function () return ExtendedCharacterStats.profile.spellBonus.shadowDmg.display; end,
                         set = function (_, value)
                             ExtendedCharacterStats.profile.spellBonus.shadowDmg.display = value
