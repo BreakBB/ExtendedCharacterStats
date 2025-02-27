947fede17f0180bccb5d9da0ced3e12c113ce370
Breno Ludgero <116920906+BrenoLudgero@users.noreply.github.com>
2025-02-27 16:13:56 +0000

    [feature] Add setting to change item quality colors' intensity (Thanks @BrenoLudgero)

--- Modules/Migration.lua
+++ Modules/Migration.lua
@@ -14,4 +14,9 @@ function Migration:ToLatestProfileVersion(profileVersion)
         ECS:Print(i18n("Profile has been reset due to a major update.")) -- v4.0.0 because of major spell restructuring
         return
     end
+
+    local defaultProfile = Profile:GetDefaultProfile()
+    if profileVersion < 21 then
+        ExtendedCharacterStats.general.qualityColorsIntensity = defaultProfile.general.qualityColorsIntensity
+    end
 end
