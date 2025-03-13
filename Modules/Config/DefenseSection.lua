503214eb9008f5c5570270cb697479d440b2797f
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2025-03-13 03:44:07 +0100

    fixes

--- Modules/Config/DefenseSection.lua
+++ Modules/Config/DefenseSection.lua
@@ -98,7 +98,7 @@ function _Config:LoadDefenseSection()
                 name = function() return i18n("Avoidance (Lvl +3)") end,
                 desc = function() return i18n("Shows/Hides the total avoidance (Lvl +3).") end,
                 width = 1.5,
-                disabled = function() return (not ExtendedCharacterStats.profile.defenseBoss.display); end,
+                disabled = function() return (not ExtendedCharacterStats.profile.defense.display); end,
                 get = function () return ExtendedCharacterStats.profile.defense.avoidanceBoss.display; end,
                 set = function (_, value)
                     ExtendedCharacterStats.profile.defense.avoidanceBoss.display = value
