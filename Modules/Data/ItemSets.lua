766c38a41baeb091bf67915d72c202e08fd9e5af
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2025-03-16 19:07:30 +0100

    wip

--- Modules/Data/ItemSets.lua
+++ Modules/Data/ItemSets.lua
@@ -5,6 +5,13 @@ local _, _, classId = UnitClass("player")
 local setNames = Data.setNames
 
 local itemSets = {
+    [setNames.AUGURS_REGALIA] = {
+        [19609] = true,
+        [19828] = true,
+        [19829] = true,
+        [19830] = true,
+        [19956] = true
+    },
     [setNames.BATTLEGEAR_OF_MIGHT] = {
         [16861] = true,
         [16862] = true,
@@ -14,79 +21,6 @@ local itemSets = {
         [16866] = true,
         [16867] = true,
         [16868] = true
-    },
-    [setNames.STORMRAGE_RAIMENT] = {
-        [16899] = true,
-        [16900] = true,
-        [16901] = true,
-        [16902] = true,
-        [16903] = true,
-        [16904] = true,
-        [16897] = true,
-        [16898] = true
-    },
-    [setNames.VESTMENTS_OF_TRANSCENDENCE] = {
-        [16919] = true,
-        [16920] = true,
-        [16921] = true,
-        [16922] = true,
-        [16923] = true,
-        [16924] = true,
-        [16925] = true,
-        [16926] = true
-    },
-    [setNames.TEN_STORMS] = {
-        [16943] = true,
-        [16944] = true,
-        [16945] = true,
-        [16946] = true,
-        [16947] = true,
-        [16948] = true,
-        [16949] = true,
-        [16950] = true
-    },
-    [setNames.THE_EARTHSHATTERER] = {
-        [22468] = true,
-        [22470] = true,
-        [22469] = true,
-        [22466] = true,
-        [22465] = true,
-        [22467] = true,
-        [22464] = true,
-        [22471] = true,
-        [23065] = true
-    },
-    [setNames.HARUSPEXS_GARB] = {
-        [19613] = true,
-        [19838] = true,
-        [19839] = true,
-        [19840] = true,
-        [19955] = true
-    },
-    [setNames.AUGURS_REGALIA] = {
-        [19609] = true,
-        [19828] = true,
-        [19829] = true,
-        [19830] = true,
-        [19956] = true
-    },
-    [setNames.FREETHINKERS_ARMOR] = {
-        [19588] = true,
-        [19825] = true,
-        [19826] = true,
-        [19827] = true,
-        [19952] = true
-    },
-    [setNames.DREADNOUGHTS_BATTLEGEAR] = {
-        [236005] = true,
-        [246006] = true,
-        [236007] = true,
-        [236008] = true,
-        [236009] = true,
-        [236010] = true,
-        [236011] = true,
-        [236012] = true,
-        [236013] = true
     },
     [setNames.BONESCYTHE_LEATHERS] = {
         [236032] = true,
@@ -99,38 +33,16 @@ local itemSets = {
         [236039] = true,
         [236040] = true
     },
-    [setNames.THE_EARTHSHATTERERS_RESOLVE] = {
-        [236160] = true,
-        [236162] = true,
-        [236163] = true,
-        [236164] = true,
-        [236165] = true,
-        [236166] = true,
-        [236167] = true,
-        [236168] = true,
-        [236169] = true
-    },
-    [setNames.REDEMPTION_BULWARK] = {
-        [236133] = true,
-        [236134] = true,
-        [236135] = true,
-        [236136] = true,
-        [236137] = true,
-        [236138] = true,
-        [236139] = true,
-        [236140] = true,
-        [236141] = true
-    },
-    [setNames.PLAGUEHEART_STITCHINGS] = {
-        [236068] = true,
-        [236069] = true,
-        [236070] = true,
-        [236071] = true,
-        [236072] = true,
-        [236073] = true,
-        [236074] = true,
-        [236075] = true,
-        [236076] = true
+    [setNames.DREADNOUGHTS_BATTLEGEAR] = {
+        [236005] = true,
+        [246006] = true,
+        [236007] = true,
+        [236008] = true,
+        [236009] = true,
+        [236010] = true,
+        [236011] = true,
+        [236012] = true,
+        [236013] = true
     },
     [setNames.DREAMWALKER_GUARDIAN] = {
         [236201] = true,
@@ -143,9 +55,102 @@ local itemSets = {
         [236208] = true,
         [236209] = true
     },
+    [setNames.FREETHINKERS_ARMOR] = {
+        [19588] = true,
+        [19825] = true,
+        [19826] = true,
+        [19827] = true,
+        [19952] = true
+    },
+    [setNames.HARUSPEXS_GARB] = {
+        [19613] = true,
+        [19838] = true,
+        [19839] = true,
+        [19840] = true,
+        [19955] = true
+    },
+    [setNames.PLAGUEHEART_STITCHINGS] = {
+        [236068] = true,
+        [236069] = true,
+        [236070] = true,
+        [236071] = true,
+        [236072] = true,
+        [236073] = true,
+        [236074] = true,
+        [236075] = true,
+        [236076] = true
+    },
+    [setNames.REDEMPTION_BULWARK] = {
+        [236133] = true,
+        [236134] = true,
+        [236135] = true,
+        [236136] = true,
+        [236137] = true,
+        [236138] = true,
+        [236139] = true,
+        [236140] = true,
+        [236141] = true
+    },
+    [setNames.STORMRAGE_RAIMENT] = {
+        [16899] = true,
+        [16900] = true,
+        [16901] = true,
+        [16902] = true,
+        [16903] = true,
+        [16904] = true,
+        [16897] = true,
+        [16898] = true
+    },
+    [setNames.SYMBOLS_OF_UNENDING_LIFE] = {
+        [233417] = true,
+        [233418] = true,
+        [233419] = true
+    },
+    [setNames.TEN_STORMS] = {
+        [16943] = true,
+        [16944] = true,
+        [16945] = true,
+        [16946] = true,
+        [16947] = true,
+        [16948] = true,
+        [16949] = true,
+        [16950] = true
+    },
+    [setNames.THE_EARTHSHATTERER] = {
+        [22464] = true,
+        [22465] = true,
+        [22466] = true,
+        [22467] = true,
+        [22468] = true,
+        [22469] = true,
+        [22470] = true,
+        [22471] = true,
+        [23065] = true
+    },
+    [setNames.THE_EARTHSHATTERERS_RESOLVE] = {
+        [236160] = true,
+        [236162] = true,
+        [236163] = true,
+        [236164] = true,
+        [236165] = true,
+        [236166] = true,
+        [236167] = true,
+        [236168] = true,
+        [236169] = true
+    },
+    [setNames.VESTMENTS_OF_TRANSCENDENCE] = {
+        [16919] = true,
+        [16920] = true,
+        [16921] = true,
+        [16922] = true,
+        [16923] = true,
+        [16924] = true,
+        [16925] = true,
+        [16926] = true
+    },
 }
 
-function Data:HasSetBonusIncreasedExperise()
+function Data:HasSetBonusIncreasedExpertise2()
     if classId == Data.WARRIOR then
         return Data:IsSetBonusActive(setNames.DREADNOUGHTS_BATTLEGEAR, 2)
     elseif classId == Data.DRUID then
@@ -158,6 +163,12 @@ function Data:HasSetBonusIncreasedExperise()
         return Data:IsSetBonusActive(setNames.BONESCYTHE_LEATHERS, 2)
     elseif classId == Data.WARLOCK then
         return Data:IsSetBonusActive(setNames.PLAGUEHEART_STITCHINGS, 2)
+    end
+end
+
+function Data:HasSetBonusIncreasedExpertise5()
+    if classId == Data.DRUID then
+        return Data:IsSetBonusActive(setNames.SYMBOLS_OF_UNENDING_LIFE, 3)
     end
 end
 
