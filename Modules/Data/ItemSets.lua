2532570abddc24ac4a275f4766ca009914dfcd34
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2025-04-08 18:38:52 +0200

    [fix] Handle more MP5 bonuses from gear sets
    Fixes #365

--- Modules/Data/ItemSets.lua
+++ Modules/Data/ItemSets.lua
@@ -5,6 +5,13 @@ local _, _, classId = UnitClass("player")
 local setNames = Data.setNames
 
 local itemSets = {
+    [setNames.AUGURS_REGALIA] = {
+        [19609] = true,
+        [19828] = true,
+        [19829] = true,
+        [19830] = true,
+        [19956] = true
+    },
     [setNames.BATTLEGEAR_OF_MIGHT] = {
         [16861] = true,
         [16862] = true,
@@ -15,6 +22,41 @@ local itemSets = {
         [16867] = true,
         [16868] = true
     },
+    [setNames.BLOODSOUL_EMBRACE] = {
+        [19690] = true,
+        [19691] = true,
+        [19692] = true,
+    },
+    [setNames.FEL_IRON_CHAIN] = {
+        [23490] = true,
+        [23491] = true,
+        [23494] = true,
+        [23493] = true,
+    },
+    [setNames.FREETHINKERS_ARMOR] = {
+        [19588] = true,
+        [19825] = true,
+        [19826] = true,
+        [19827] = true,
+        [19952] = true
+    },
+    [setNames.GREEN_DRAGON_MAIL] = {
+        [15045] = true,
+        [15046] = true,
+        [20296] = true,
+    },
+    [setNames.HARUSPEXS_GARB] = {
+        [19613] = true,
+        [19838] = true,
+        [19839] = true,
+        [19840] = true,
+        [19955] = true
+    },
+    [setNames.LIVING_GREEN_DRAGON_MAIL] = {
+        [227879] = true,
+        [227878] = true,
+        [227877] = true,
+    },
     [setNames.STORMRAGE_RAIMENT] = {
         [16899] = true,
         [16900] = true,
@@ -24,16 +66,6 @@ local itemSets = {
         [16904] = true,
         [16897] = true,
         [16898] = true
-    },
-    [setNames.VESTMENTS_OF_TRANSCENDENCE] = {
-        [16919] = true,
-        [16920] = true,
-        [16921] = true,
-        [16922] = true,
-        [16923] = true,
-        [16924] = true,
-        [16925] = true,
-        [16926] = true
     },
     [setNames.TEN_STORMS] = {
         [16943] = true,
@@ -56,45 +88,87 @@ local itemSets = {
         [22471] = true,
         [23065] = true
     },
-    [setNames.HARUSPEXS_GARB] = {
-        [19613] = true,
-        [19838] = true,
-        [19839] = true,
-        [19840] = true,
-        [19955] = true
+    [setNames.VESTMENTS_OF_TRANSCENDENCE] = {
+        [16919] = true,
+        [16920] = true,
+        [16921] = true,
+        [16922] = true,
+        [16923] = true,
+        [16924] = true,
+        [16925] = true,
+        [16926] = true
     },
-    [setNames.AUGURS_REGALIA] = {
-        [19609] = true,
-        [19828] = true,
-        [19829] = true,
-        [19830] = true,
-        [19956] = true
-    },
-    [setNames.FREETHINKERS_ARMOR] = {
-        [19588] = true,
-        [19825] = true,
-        [19826] = true,
-        [19827] = true,
-        [19952] = true
+    [setNames.WINDHAWK_ARMOR] = {
+        [29522] = true,
+        [29523] = true,
+        [29524] = true,
     },
 }
 
 function Data:HasSetBonusModifierMP5()
-    if classId == Data.PRIEST then
-        return Data:IsSetBonusActive(setNames.VESTMENTS_OF_TRANSCENDENCE, 3)
-    elseif classId == Data.DRUID then
-        return Data:IsSetBonusActive(setNames.STORMRAGE_RAIMENT, 3)
-    end
+    return (ECS.IsClassic and (
+        Data:IsSetBonusActive(setNames.GREEN_DRAGON_MAIL, 3) or
+        (classId == Data.DRUID and Data:IsSetBonusActive(setNames.STORMRAGE_RAIMENT, 3)) or
+        (classId == Data.PRIEST and Data:IsSetBonusActive(setNames.VESTMENTS_OF_TRANSCENDENCE, 3)) or
+        (ECS.IsSod and Data:IsSetBonusActive(setNames.LIVING_GREEN_DRAGON_MAIL, 3))
+    ))
 end
 
 function Data:GetSetBonusValueMP5()
-    if (classId == Data.DRUID and Data:IsSetBonusActive(setNames.HARUSPEXS_GARB, 2))
-        or (classId == Data.SHAMAN and Data:IsSetBonusActive(setNames.AUGURS_REGALIA, 2))
-        or (classId == Data.PALADIN and Data:IsSetBonusActive(setNames.FREETHINKERS_ARMOR, 2)) then
-        return 4
+    local bonus = 0
+    if (classId == Data.DRUID and Data:IsSetBonusActive(setNames.HARUSPEXS_GARB, 2)) or
+        (classId == Data.SHAMAN and Data:IsSetBonusActive(setNames.AUGURS_REGALIA, 2)) or
+        (classId == Data.PALADIN and Data:IsSetBonusActive(setNames.FREETHINKERS_ARMOR, 2)
+    ) then
+        if ECS.IsWotlk then
+            bonus = bonus + 5
+        else
+            bonus = bonus + 4
+        end
     end
-
-    return 0
+    if ((ECS.IsSod and Data:IsSetBonusActive(setNames.LIVING_GREEN_DRAGON_MAIL, 2)) or
+        Data:IsSetBonusActive(setNames.GREEN_DRAGON_MAIL, 2)
+    ) then
+        if ECS.IsWotlk then
+            bonus = bonus + 4
+        else
+            bonus = bonus + 3
+        end
+    end
+    if (not ECS.IsClassic and (
+            Data:IsSetBonusActive(setNames.GREEN_DRAGON_MAIL, 3) or
+            (classId == Data.DRUID and Data:IsSetBonusActive(setNames.STORMRAGE_RAIMENT, 3)) or
+            (classId == Data.PRIEST and Data:IsSetBonusActive(setNames.VESTMENTS_OF_TRANSCENDENCE, 3))
+        )
+    ) then
+        if ECS.IsWotlk then
+            bonus = bonus + 25
+        elseif ECS.IsTbc then
+            bonus = bonus + 20
+        end
+    end
+    if Data:IsSetBonusActive(setNames.BLOODSOUL_EMBRACE, 3) then
+        if ECS.IsWotlk then
+            bonus = bonus + 15
+        else
+            bonus = bonus + 12
+        end
+    end
+    if Data:IsSetBonusActive(setNames.FEL_IRON_CHAIN, 4) then
+        if ECS.IsWotlk then
+            bonus = bonus + 10
+        else
+            bonus = bonus + 8
+        end
+    end
+    if Data:IsSetBonusActive(setNames.WINDHAWK_ARMOR, 3) then
+        if ECS.IsWotlk then
+            bonus = bonus + 10
+        else
+            bonus = bonus + 8
+        end
+    end
+    return bonus
 end
 
 function Data:HasNatureCritBonusModifier()
