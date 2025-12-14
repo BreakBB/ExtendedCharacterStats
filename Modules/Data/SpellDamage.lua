e0e29fa241756f45b0f0db3320206be896e7febe
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2025-12-14 05:03:43 +0100

    Remove Arcane Power talent from spell damage calculation

--- Modules/Data/SpellDamage.lua
+++ Modules/Data/SpellDamage.lua
@@ -3,30 +3,11 @@ local Data = ECSLoader:ImportModule("Data")
 ---@type DataUtils
 local DataUtils = ECSLoader:ImportModule("DataUtils")
 
-local _SpellDamage = {}
-
-local _, _, classId = UnitClass("player")
-
 ---@param school number
 ---@return number
 function Data:GetSpellDamage(school)
     local spellDmg = GetSpellBonusDamage(school)
-    local modifier = _SpellDamage:GetGeneralTalentModifier()
-    spellDmg = spellDmg * (1 + (modifier / 100))
     return DataUtils:Round(spellDmg, 0)
-end
-
----@return number
-function _SpellDamage:GetGeneralTalentModifier()
-    local mod = 0
-
-    if classId == Data.MAGE then
-        local talentSlot = ECS.IsWotlk and 17 or 14
-        local _, _, _, _, points, _, _, _ = GetTalentInfo(1, talentSlot)
-        mod = points * 1 -- 0-3% Arcane Instability
-    end
-
-    return mod
 end
 
 ---@return number
