6d124a26d72b8ee2b3bc96041570ab8632d9238b
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2025-12-25 17:12:19 +0100

    wip

--- Modules/Data/SpellDamage.lua
+++ Modules/Data/SpellDamage.lua
@@ -49,12 +49,40 @@ end
 function Data:GetSpellHasteBonus()
     local hasteBonus = GetHaste()
 
+    -- items
     local timeworn = DataUtils:CountTimewornItems()
-
     for i = 1, 18 do
         local id, _ = GetInventoryItemID("player", i)
         hasteBonus = hasteBonus + (Data.Item.SpellHaste[id] or 0)
         hasteBonus = hasteBonus + hasteBonus * (Data.Item.TimewornSpellHaste[id] or 0)
+    end
+
+    -- buffs
+    i = 1
+    repeat
+        local aura = C_UnitAuras.GetBuffDataByIndex("player", i)
+        if aura and aura.spellId then
+            hasteBonus = hasteBonus + (Data.Aura.SpellHaste[aura.spellId] or 0)
+        end
+        i = i + 1
+    until (not aura)
+
+    -- debuffs
+    i = 1
+    repeat
+        local aura = C_UnitAuras.GetDebuffDataByIndex("player", i)
+        if aura and aura.spellId then
+            hasteBonus = hasteBonus + (Data.Aura.SpellHaste[aura.spellId] or 0)
+        end
+        i = i + 1
+    until (not aura)
+
+    -- not stacking buffs
+    if ECS.IsSod then
+        aura = C_UnitAuras.GetPlayerAuraBySpellID(1219557) -- Power of the Guardian
+        if aura then
+            hasteBonus = hasteBonus + 2
+        end
     end
 
     return DataUtils:Round(hasteBonus, 2) .. "%"
