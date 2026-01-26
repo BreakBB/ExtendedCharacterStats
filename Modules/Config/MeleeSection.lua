e0b4b3f58c6955ed3c681f80ad3854171bac62ab
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2026-01-26 17:59:00 +0100

    rework conditional
    Co-authored-by: Björn Heiß <b.boeing@online.de>

--- Modules/Config/MeleeSection.lua
+++ Modules/Config/MeleeSection.lua
@@ -92,7 +92,7 @@ function _Config:LoadMeleeSection()
                 desc = function() return i18n("Shows/Hides the expertise value.") end,
                 width = 1.5,
                 hidden = function()
-                    return not (ECS.IsTbc or ECS.IsWotlk or ECS.IsSod)
+                    return ECS.IsClassic and (not ECS.IsSoD)
                 end,
                 disabled = function() return (not ExtendedCharacterStats.profile.melee.display); end,
                 get = function () return ExtendedCharacterStats.profile.melee.expertise.display; end,
