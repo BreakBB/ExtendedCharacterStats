e9e00e4e303fc8b9edb2480405a31c1e01681314
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2026-08-01 20:36:10 +0200

    missing end

--- Modules/Migration.lua
+++ Modules/Migration.lua
@@ -28,6 +28,7 @@ function Migration:ToLatestProfileVersion(profileVersion)
         ExtendedCharacterStats.profile.spellBonus.baseSpellDmg = defaultProfile.profile.spellBonus.baseSpellDmg
         ExtendedCharacterStats.profile.defense.enemyMissChance = defaultProfile.profile.defense.enemyMissChance
         ExtendedCharacterStats.profile.defense.enemyMissChanceBoss = defaultProfile.profile.defense.enemyMissChanceBoss
+    end
     if profileVersion < 27 then
         ExtendedCharacterStats.profile.regen.mp5Buffs = nil
         ExtendedCharacterStats.profile.regen.mp5Items = nil
