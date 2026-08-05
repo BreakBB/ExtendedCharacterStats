699e9bfd1e054d53bc0521deef2e0083a3e26cc3
Alessandro Barbieri <lssndrbarbieri@gmail.com>
2026-08-06 00:17:59 +0200

    Remove Elemental Devastation from spellcrit
    Elemental Devastation gives melee+ranged crit

--- Modules/Data/Constants.lua
+++ Modules/Data/Constants.lua
@@ -286,9 +286,6 @@ Data.Aura = {
     },
     SpellCrit = {
         [24907] = ((not ECS.IsClassic) and 5 or nil), -- Moonkin Aura
-        [29177] = 6, -- Elemental Devastation Rank 2
-        [29178] = 9, -- Elemental Devastation Rank 3
-        [30165] = 3, -- Elemental Devastation Rank 1
         [51466] = 3, -- Elemental Oath Rank 1
         [51470] = 5, -- Elemental Oath Rank 2
     },
