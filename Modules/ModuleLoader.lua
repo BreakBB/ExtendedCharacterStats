5f696a927d5d7e54e7f1a5ddb28b80a4e0cfecae
BreakBB <b.boeing@online.de>
2026-08-05 22:16:14 +0200

    Fix ECSLoader typings

--- Modules/ModuleLoader.lua
+++ Modules/ModuleLoader.lua
@@ -1,34 +1,30 @@
 ---@class ECSLoader
 ECSLoader = {}
 
--- ["ModuleName"] = moduleReference
+---@class Module
+---@field public private table -- TODO: We need to re-think the "private" module part
+
 ---@type table<string, Module>
 local modules = {}
 
----@return Module @Module reference
-local function _CreateBlankModule()
-    ---@class Module
-    local module = {}
-    module.private = {}
-    return module
-end
-
----@param name string @Module name
----@return Module @Module reference
+---@generic T : Module
+---@param name `T` @Module name
+---@return T @Module reference
 function ECSLoader:CreateModule(name)
   if not modules[name] then
-    modules[name] = _CreateBlankModule()
+    modules[name] = { private = {} }
     return modules[name]
   else
     return modules[name]
   end
 end
 
----@param name string @Module name
----@return Module @Module reference
+---@generic T : Module
+---@param name `T` @Module name
+---@return T @Module reference
 function ECSLoader:ImportModule(name)
   if not modules[name] then
-    modules[name] = _CreateBlankModule()
+    modules[name] = { private = {} }
     return modules[name]
   else
     return modules[name]
